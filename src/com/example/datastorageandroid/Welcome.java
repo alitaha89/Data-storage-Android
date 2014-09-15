29ca283f7acf2bb8cf8177b920b242299588ae65
alitaha89 <taha.ali.1989@gmail.com>
2014-09-15 13:11:20 +0300

    initiate app with welcome activity

---
+++ src/com/example/datastorageandroid/Welcome.java
@@ -0,0 +1,27 @@
+package com.example.datastorageandroid;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Welcome extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle AliTaha) {
+		// TODO Auto-generated method stub
+		super.onCreate(AliTaha);
+		setContentView(R.layout.welcome);
+	
+	}
+	// TODO Auto-generated method stub  test comit
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
+	
+
+}
