1d7d26e23872a814cfe7ccd8284a4f2d239d6582
alitaha89 <taha.ali.1989@gmail.com>
2014-09-16 12:09:19 +0300

    save_data_into_online_file xml file

--- gen/com/example/datastorageandroid/R.java
+++ gen/com/example/datastorageandroid/R.java
@@ -1200,9 +1200,10 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
         public static final int save_data_into_internal_file=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
-        public static final int view_internal_file_data=0x7f03001b;
-        public static final int welcome=0x7f03001c;
+        public static final int save_data_into_online_file=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int view_internal_file_data=0x7f03001c;
+        public static final int welcome=0x7f03001d;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
