f19f832da543ef91d1b134537c97f9556b74a42f
alitaha89 <taha.ali.1989@gmail.com>
2014-09-18 18:42:40 +0300

    note list activity .java+xml

--- gen/com/example/datastorageandroid/R.java
+++ gen/com/example/datastorageandroid/R.java
@@ -1092,13 +1092,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button=0x7f05003d;
-        public static final int buttonempty=0x7f05003f;
-        public static final int buttonview=0x7f05003e;
+        public static final int button=0x7f05003e;
+        public static final int buttonempty=0x7f050040;
+        public static final int buttonview=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1120,6 +1120,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int note_list=0x7f05003c;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1140,8 +1141,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int texttosave=0x7f05003c;
-        public static final int texttoview=0x7f050040;
+        public static final int texttosave=0x7f05003d;
+        public static final int texttoview=0x7f050041;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1199,15 +1200,16 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int cloud_storage=0x7f030019;
-        public static final int save_data_into_internal_file=0x7f03001a;
-        public static final int save_data_into_mysqllite_database=0x7f03001b;
-        public static final int save_data_into_online_file=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
-        public static final int view_internal_file_data=0x7f03001e;
-        public static final int view_mysqllite_data=0x7f03001f;
-        public static final int view_online_file_data=0x7f030020;
-        public static final int welcome=0x7f030021;
+        public static final int activity_note_list=0x7f030019;
+        public static final int cloud_storage=0x7f03001a;
+        public static final int save_data_into_internal_file=0x7f03001b;
+        public static final int save_data_into_mysqllite_database=0x7f03001c;
+        public static final int save_data_into_online_file=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
+        public static final int view_internal_file_data=0x7f03001f;
+        public static final int view_mysqllite_data=0x7f030020;
+        public static final int view_online_file_data=0x7f030021;
+        public static final int welcome=0x7f030022;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
